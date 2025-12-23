dc979a24e5aaf7d8bb3461add304bb9fce6bb657
elreviae <elreviae@gmail.com>
2025-12-23 18:10:36 +0100

    Update addition models.py for "class Site(models.Model)" & "class Employee(models.Model)" + Update of "class InventoryItem(models.Model)" for "assigned_to" & "site"

--- apps/home/models.py
+++ apps/home/models.py
@@ -7,6 +7,32 @@ from django.db import models
 # from django.contrib.auth.models import User
 
 # Create your models here.
+class Site(models.Model):
+    name = models.CharField(max_length=100, unique=True, verbose_name="Nom du site")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Site"
+        verbose_name_plural = "Sites"
+
+class Employee(models.Model):
+    full_name = models.CharField(max_length=200, verbose_name="Nom complet de l'employé")
+    # Un employé peut appartenir à plusieurs sites
+    sites = models.ManyToManyField(Site, verbose_name="Sites d'affectation")
+
+    def __str__(self):
+        # Affiche le nom et ses sites pour aider l'admin
+        # sites_list = ", ".join([site.name for site in self.sites.all()])
+        # return f"{self.full_name} ({sites_list})"
+        return f"{self.full_name}"
+
+    class Meta:
+        verbose_name = "Employé"
+        verbose_name_plural = "Employés"
+        ordering = ['full_name']
+
 class EquipmentType(models.Model):
     name = models.CharField(max_length=100, unique=True, verbose_name="Type d'équipement")
 
@@ -34,14 +60,29 @@ class InventoryItem(models.Model):
     brand = models.ForeignKey(Brand, on_delete=models.PROTECT, verbose_name="Marque")
     model = models.CharField(max_length=100, verbose_name="Modèle")
     serial_number = models.CharField(max_length=100, unique=True, verbose_name="Numéro de série")
-    assigned_to = models.CharField(max_length=200, blank=True, null=True, verbose_name="Attribué à")
+    # null=True permet à la base d'accepter du vide
+    # blank=True permet au formulaire de laisser le champ vide
+    assigned_to = models.ForeignKey(
+        Employee,
+        on_delete=models.SET_NULL,  # Si l'employé est supprimé, l'objet reste mais devient libre
+        blank=True,
+        null=True,
+        verbose_name="Attribué à"
+    )
+    site = models.ForeignKey(
+        Site,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+        verbose_name="Site"
+    )
     purchase_date = models.DateField(blank=True, null=True, verbose_name="Date d'achat")
     notes = models.TextField(blank=True, null=True, verbose_name="Notes")
 
     def __str__(self):
-        return f"{self.brand} {self.model} ({self.serial_number})"
+        return f"{self.site}  {self.brand} {self.model} ({self.serial_number})"
 
     class Meta:
         verbose_name = "Élément d'inventaire"
         verbose_name_plural = "Éléments d'inventaire"
-        ordering = ['brand', 'model']
+        ordering = ['brand', 'model', 'site']
